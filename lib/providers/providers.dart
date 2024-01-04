9b2bea6546fb7efb4aba93e25bd0129e6a1f40a6
laksh29 <laksh.samdariya.ai@ghrce.raisoni.net>
2024-01-05 00:30:48 +0530

    removed : unwanted providers

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -2,11 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../utils/seat.dart';
 
-final findController = StateProvider<int>((ref) => 0);
-
-final searchSeat = StateProvider<int>((ref) => 0);
-
-final selectedSeats = StateProvider<List>((ref) => []);
+final searchSeatProvider = StateProvider<int>((ref) => 0);
 
 final seatsSelectedProvider =
     StateNotifierProvider<SeatsSelected, List>((ref) => SeatsSelected());
@@ -15,8 +11,13 @@ class SeatsSelected extends StateNotifier<List> {
   SeatsSelected() : super([]);
 
   void selectSeats(Seat seat) {
-    state.add(seat.index);
+    state = [...state, seat.index];
   }
 
-  void unselectSeats(Seat seat) => state.remove(seat.index);
+  void unselectSeats(Seat seat) {
+    state = [
+      for (var index in state)
+        if (index != seat.index) index,
+    ];
+  }
 }

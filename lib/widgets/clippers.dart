60b00fef88c93f77e35ce5c33c4cb24874cc9c44
laksh29 <laksh.samdariya.ai@ghrce.raisoni.net>
2024-01-05 00:49:08 +0530

    feat : created custolm clippers for example seats

--- lib/widgets/clippers.dart
+++ lib/widgets/clippers.dart
@@ -21,3 +21,25 @@ class SeatClipper extends CustomClipper<Path> {
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
+
+class DisplayClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    double h = size.height;
+    double w = size.width;
+    Path path = Path()
+      ..moveTo(0, h)
+      ..lineTo(0, 0)
+      ..lineTo(w, 0)
+      ..lineTo(w, h)
+      ..lineTo(w - 5, h)
+      ..lineTo(w - 5, 5)
+      ..lineTo(5, 5)
+      ..lineTo(5, h)
+      ..close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}

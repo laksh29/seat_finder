9bedbd2e5d86585f384969db68821343b87adb08
laksh29 <laksh.samdariya.ai@ghrce.raisoni.net>
2024-01-05 00:48:56 +0530

    feat : created example seats

---
+++ lib/widgets/example_seats.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../constants/sized_box.dart';
+import 'clippers.dart';
+
+class ExampleSeats extends StatelessWidget {
+  const ExampleSeats({
+    super.key,
+    required this.color,
+    required this.text,
+    this.clipped = false,
+  });
+  final String text;
+  final Color? color;
+  final bool clipped;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        clipped
+            ? ClipPath(
+                clipper: DisplayClipper(),
+                child: Container(
+                  height: 25,
+                  width: 25,
+                  decoration: BoxDecoration(
+                    color: color,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              )
+            : Container(
+                height: 25,
+                width: 25,
+                decoration: BoxDecoration(
+                  color: color,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+        buildHeight(5),
+        Text(
+          text,
+          style: const TextStyle(fontSize: 10),
+        )
+      ],
+    );
+  }
+}

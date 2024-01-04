68d1dc0f91525a4b5d016e082db08417fd189eb3
laksh29 <laksh.samdariya.ai@ghrce.raisoni.net>
2024-01-05 00:27:55 +0530

    style : added new colors for emergency and selected

--- lib/constants/color_constants.dart
+++ lib/constants/color_constants.dart
@@ -3,4 +3,7 @@ import 'package:flutter/material.dart';
 class ColorConstants {
   static Color? dark = Colors.blue[900];
   static Color? light = Colors.blue[50];
+  static Color? emergency = Colors.red[500];
+  static Color? selected = Colors.blue[500];
+  static Color? searched = Colors.cyan[200];
 }
